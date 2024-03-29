63611e0bf699beeb1a47b7ca33a35f5e3a3bd5da
Evan Chen <evan@evanchen.cc>
2024-03-29 00:31:06 -0400

    Sync retreat notes too

--- sync.sh
+++ sync.sh
@@ -4,4 +4,5 @@ set -euxo pipefail
 
 latexmk
 gsutil cp notes.pdf gs://web.evanchen.cc/upload/0xparc-notes-sp2024.pdf
-gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/0xparc-notes-sp2024.pdf
+gsutil cp retreat-notes.pdf gs://web.evanchen.cc/upload/0xparc-retreat-sp2024.pdf
+gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/0xparc-retreat-sp2024.pdf
