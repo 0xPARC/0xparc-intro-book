733bce11db88bf476aba26fd756b0f5336151756
Evan Chen <evan@evanchen.cc>
2024-03-29 14:08:15 -0400

    Move Evan's stuff into a separate subdir

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 
 latexmk
 typst compile retreat-notes.typ
-gsutil cp notes.pdf gs://web.evanchen.cc/upload/0xparc-notes-sp2024.pdf
-gsutil cp retreat-notes.pdf gs://web.evanchen.cc/upload/0xparc-retreat-sp2024.pdf
+gsutil cp evan/evan-pcp-old-notes.pdf gs://web.evanchen.cc/upload/0xparc-notes-sp2024.pdf
+gsutil cp evan/evan-retreat-notes.pdf gs://web.evanchen.cc/upload/0xparc-retreat-sp2024.pdf
+gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/0xparc-notes-sp2024.pdf
 gsutil setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/0xparc-retreat-sp2024.pdf
